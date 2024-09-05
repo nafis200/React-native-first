fbfd90fecbabc9fc0866b5b79393a053558d5895
nafis200 <nafisahamed14@gmail.com>
2024-09-05 11:23:21 +0600

    space-padding add

--- components1/Title.jsx
+++ components1/Title.jsx
@@ -1,5 +1,5 @@
 
-import { View, Text,SafeAreaView } from 'react-native'
+import { View, Text,SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import PropTypes from "prop-types"
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
@@ -7,11 +7,15 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 const Title = (props) => {
   return (
     <SafeAreaView>
+      <ScrollView>
+      <View style={{flex:1, flexDirection:'row', alignItems: 'center',justifyContent:'space-between',paddingTop:30,paddingLeft:17,paddingRight:26}}>
       <Text style={{fontSize:30}}>{props.title}</Text>
       <FontAwesomeIcon icon={faEnvelope}
        style={{color:'white'}}
        size={32}
       />
+      </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
