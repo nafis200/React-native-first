22849df43445102fc672b36c4676c4f20da8ad14
nafis200 <nafisahamed14@gmail.com>
2024-09-05 15:47:15 +0600

    pagination done

--- components1/Title.jsx
+++ components1/Title.jsx
@@ -1,86 +1,105 @@
+import { View, Text, SafeAreaView, ScrollView, Pressable, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import UserStory from './UserStory';
 
-import { View, Text,SafeAreaView, ScrollView, Pressable, FlatList } from 'react-native'
-import React, { useState } from 'react'
-import PropTypes from "prop-types"
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import UserStory from './UserStory'
 const Title = (props) => {
   const data = [
-    {
-       firstName : 'Joseph',
-       id:1,
-    },
-    {
-       firstName : 'Angel',
-       id:2,
-    },
-    {
-       firstName : 'White',
-       id:3,
-    },
-    {
-       firstName : 'Joseph',
-       id:4,
-    },
-    {
-       firstName : 'Angel',
-       id:5,
-    },
-    {
-       firstName : 'White',
-       id:6,
-    },
-    {
-       firstName : 'Joseph',
-       id:7,
-    },
-    {
-       firstName : 'Angel',
-       id:8,
-    },
-    {
-       firstName : 'White',
-       id:9,
-    },
-  ]
+    { firstName: 'Joseph', id: 1 },
+    { firstName: 'Angel', id: 2 },
+    { firstName: 'White', id: 3 },
+    { firstName: 'Joseph', id: 4 },
+    { firstName: 'Angel', id: 5 },
+    { firstName: 'White', id: 6 },
+    { firstName: 'Joseph', id: 7 },
+    { firstName: 'Angel', id: 8 },
+    { firstName: 'White', id: 9 },
+  ];
 
-  const pageSIze = 4 
-  const [pageNumber,setPageNumber] = useState(1)
-  const [isLoading,setIsLoading] = useState(false)
-  const [renderData, setRenderData] = useState([])
+  const pageSize = 4;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [renderData, setRenderData] = useState(data.slice(0, pageSize));
+
+  const pagination = (data, pageNumber, pageSize) => {
+    let startIndex = (pageNumber - 1) * pageSize;
+    if (startIndex >= data.length) {
+      return [];
+    }
+    return data.slice(startIndex, startIndex + pageSize);
+  };
+
+  const loadMoreData = () => {
+    if (!isLoading) {
+      setIsLoading(true);
+      const newPageNumber = pageNumber + 1;
+      const newData = pagination(data, newPageNumber, pageSize);
+      if (newData.length > 0) {
+        setRenderData(prev => [...prev, ...newData]);
+        setPageNumber(newPageNumber);
+      }
+      setIsLoading(false);
+    }
+  };
+
   return (
     <SafeAreaView>
       <ScrollView>
-      <View style={{flex:1, flexDirection:'row', alignItems: 'center',justifyContent:'space-between',paddingTop:30,paddingLeft:17,paddingRight:26}}>
-      <Text style={{fontSize:30}}>{props.title}</Text>
-      <Pressable style={{backgroundColor:'#898DAE',padding:12, borderRadius:100}}>
-      <FontAwesomeIcon icon={faEnvelope}
-       style={{color:'white'}}
-       size={32}
-      />
-      <View style={{width:20, height:20,backgroundColor:'pink',borderRadius:10,flex:1,flexDirection:'row',alignItems:'center',justifyContent:'center',position:'absolute',right:9,top:-5}}>
-        <Text style={{fontSize:6,fontWeight:600, lineHeight:7,color:'white'}}>2</Text>
+        <View style={{
+          flex: 1, 
+          flexDirection: 'row', 
+          alignItems: 'center', 
+          justifyContent: 'space-between', 
+          paddingTop: 30, 
+          paddingLeft: 17, 
+          paddingRight: 26
+        }}>
+          <Text style={{ fontSize: 30 }}>{props.title}</Text>
+          <Pressable style={{
+            backgroundColor: '#898DAE', 
+            padding: 12, 
+            borderRadius: 100, 
+            position: 'relative'
+          }}>
+            <FontAwesomeIcon 
+              icon={faEnvelope}
+              style={{ color: 'white' }}
+              size={32}
+            />
+            <View style={{
+              width: 20, 
+              height: 20, 
+              backgroundColor: 'pink', 
+              borderRadius: 10, 
+              alignItems: 'center', 
+              justifyContent: 'center', 
+              position: 'absolute', 
+              right: 9, 
+              top: -5
+            }}>
+              <Text style={{ fontSize: 10, fontWeight: '600', lineHeight: 10, color: 'white' }}>2</Text>
+            </View>
+          </Pressable>
         </View>
-      </Pressable>
-      </View>
-      <View style={{paddingHorizontal:28}}>
-        
-      <FlatList
-        data={data}
-        horizontal={true}
-        renderItem={({item}) => <UserStory firstName={item.firstName} />}
-        keyExtractor={item => item.id}
-      />
-
-      </View>
+        <View style={{ paddingHorizontal: 28 }}>
+          <FlatList
+            keyExtractor={item => item.id.toString()}
+            onEndReachedThreshold={0.5}
+            onEndReached={loadMoreData}
+            data={renderData}
+            horizontal={true}
+            renderItem={({ item }) => <UserStory firstName={item.firstName} />}
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 Title.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
-export default Title
+export default Title;
