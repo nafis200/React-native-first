86f5b39517bff638541501858950d83f2663744a
nafis200 <nafisahamed14@gmail.com>
2024-09-05 11:11:09 +0600

    font-size

--- components1/Title.jsx
+++ components1/Title.jsx
@@ -2,10 +2,16 @@
 import { View, Text,SafeAreaView } from 'react-native'
 import React from 'react'
 import PropTypes from "prop-types"
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 const Title = (props) => {
   return (
     <SafeAreaView>
-      <Text>{props.title}</Text>
+      <Text style={{fontSize:30}}>{props.title}</Text>
+      <FontAwesomeIcon icon={faEnvelope}
+       style={{color:'white'}}
+       size={32}
+      />
     </SafeAreaView>
   )
 }
