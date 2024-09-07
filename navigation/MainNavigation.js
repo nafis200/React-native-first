5c31f5bc65636a85d6613933156f08d7a37d83a7
nafis200 <nafisahamed14@gmail.com>
2024-09-07 12:44:06 +0600

    header done

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -5,7 +5,7 @@ import Profile from '../components1/Profile';
 const Stack = createStackNavigator();
 const MainNavigation = ()=>{
      return(
-        <Stack.Navigator initialRouteName={"Home"}>
+        <Stack.Navigator initialRouteName={"Home"} screenOptions={{header: ()=> null, headerShown:false}}>
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
