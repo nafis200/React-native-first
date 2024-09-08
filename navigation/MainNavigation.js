caebc8fefda9f04f29b9104037edd7381bf4a3d7
nafis200 <nafisahamed14@gmail.com>
2024-09-08 12:56:32 +0600

    draweris done

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,14 +1,25 @@
 
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../components1/Home';
 import Profile from '../components1/Profile';
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+
+const MainMenNavigation = ()=>{
+ return <Drawer.Navigator>
+  <Drawer.Screen name="Home" component={Home} />
+  <Drawer.Screen name="Profile" component={Profile} />
+</Drawer.Navigator>
+}
+
+
 const MainNavigation = ()=>{
      return(
         <Stack.Navigator initialRouteName={"Home"} screenOptions={{header: ()=> null, headerShown:false}}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
-    </Stack.Navigator>
+      <Stack.Screen name={"Drawer"} component={MainMenNavigation} />
+      </Stack.Navigator>
     );
 }
 
